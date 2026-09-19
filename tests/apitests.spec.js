d140cc36f27f7d1d49b43234290563c0fc2d2a71
Chandni Jain <chandnij1904@gmail.com>
2026-09-19 19:11:49 +0530

    New API Changes

--- tests/apitests.spec.js
+++ tests/apitests.spec.js
@@ -2,9 +2,10 @@ import { test, expect } from '@playwright/test';
 import { REPL_MODE_STRICT } from 'repl';
 
 test("Create Booking", async({request}) => {
-const response = await request.post ('https://restful-booker.herokuapp.com/booking' , {
+
+    const response = await request.post ('https://restful-booker.herokuapp.com/booking' , {
     data: {"firstname" : "Jim","lastname" : "Brown", "totalprice" : 111, "depositpaid" : true,"bookingdates" : {
-          "checkin" : "2018-01-01",
+          "checkin" : "2018-01-01", 
           "checkout" : "2019-01-01"
         },"additionalneeds" : "Breakfast"}
     ,
@@ -19,6 +20,15 @@ const response = await request.post ('https://restful-booker.herokuapp.com/booki
     
     const bookingid = responseJSON.bookingid;
     console.log(bookingid);
+
+    expect (responseJSON.booking.firstname).toBe("Jim");
+    expect (responseJSON.booking.lastname).toBe("Brown");
+    expect (responseJSON.booking.totalprice).toBe(111);
+    expect (responseJSON.booking.depositpaid).toBe(true);
+    expect (responseJSON.booking.bookingdates.checkin).toBe("2018-01-01");
+    expect (responseJSON.booking.bookingdates.checkout).toBe("2019-01-01");
+    expect (responseJSON.booking.additionalneeds).toBe("Breakfast");
+
 });
 
 
